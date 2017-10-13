87d5bdc15a3923c001a54d152fdc4e38bd65af31
Francesco Bianchi <blanco20052@gmail.com>
2017-10-13 13:21:46 +0200

    Refactored the tests to have parameterized tests using JUnitParamsRunner

--- src/test/java/ch/lastminute/item/ItemTest.java
+++ src/test/java/ch/lastminute/item/ItemTest.java
@@ -3,19 +3,19 @@ package ch.lastminute.item;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+
+@RunWith(JUnitParamsRunner.class)
 public class ItemTest {
 
 	@Test
-	public void getShelfPriceTest() {
-		final Item item = new Item(12.49);
-		assertEquals(12.49, item.getShelfPrice(), 0);
-	}
-
-	@Test
-	public void anotherGetShelfPriceTest() {
-		final Item item = new Item(14.99);
-		assertEquals(14.99, item.getShelfPrice(), 0);
+	@Parameters({ "12.49", "14.99", "1" })
+	public void getShelfPriceTest(final double shelfPrice) {
+		final Item item = new Item(shelfPrice);
+		assertEquals(shelfPrice, item.getShelfPrice(), 0);
 	}
 
 }
