98380dae48c83e89776106ff7e6fcabec6ffb478
Francesco Bianchi <blanco20052@gmail.com>
2017-10-14 10:22:46 +0200

    refactoring tests

--- src/test/java/ch/lastminute/salestaxes/SalesTaxesTest.java
+++ src/test/java/ch/lastminute/salestaxes/SalesTaxesTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.math.BigDecimal;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import ch.lastminute.item.Item;
@@ -11,17 +12,22 @@ import ch.lastminute.item.ItemType;
 
 public class SalesTaxesTest {
 
+	SalesTaxes salesTaxes;
+
+	@Before
+	public void setUp() {
+		salesTaxes = new SalesTaxes();
+	}
+
 	@Test
 	public void bookNotImportedTest() {
 		final Item book = new Item("book", BigDecimal.valueOf(12.49), ItemType.BOOK, false);
-		final SalesTaxes salesTaxes = new SalesTaxes();
 		assertEquals(BigDecimal.valueOf(0), salesTaxes.calculateTaxes(book));
 	}
 
 	@Test
 	public void musicCdNotImportedTest() {
 		final Item cd = new Item("music CD", BigDecimal.valueOf(14.99), ItemType.OTHER, false);
-		final SalesTaxes salesTaxes = new SalesTaxes();
 		assertEquals(BigDecimal.valueOf(1.5), salesTaxes.calculateTaxes(cd));
 	}
 
