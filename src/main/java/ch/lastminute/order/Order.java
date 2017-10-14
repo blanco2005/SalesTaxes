b3c9eb3701bc227f9be099002b65e2cd67a2579f
Francesco Bianchi <blanco20052@gmail.com>
2017-10-14 14:58:26 +0200

    orders have and return size (non empty case)

--- src/main/java/ch/lastminute/order/Order.java
+++ src/main/java/ch/lastminute/order/Order.java
@@ -1,9 +1,24 @@
 package ch.lastminute.order;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import ch.lastminute.item.Item;
+
 public class Order {
 
-	public int size() {
-		return 0;
+	Set<Item> shoppingBasket;
+
+	public Order() {
+		shoppingBasket = new HashSet<>();
+	}
+
+	public void add(final Item item) {
+		shoppingBasket.add(item);
+	}
+
+	public Set<Item> getShoppingBasket() {
+		return shoppingBasket;
 	}
 
 }
--- src/test/java/ch/lastminute/order/OrderTest.java
+++ src/test/java/ch/lastminute/order/OrderTest.java
@@ -2,14 +2,26 @@ package ch.lastminute.order;
 
 import static org.junit.Assert.assertEquals;
 
+import java.math.BigDecimal;
+
 import org.junit.Test;
+
+import ch.lastminute.item.Item;
+import ch.lastminute.item.ItemType;
 
 public class OrderTest {
 
 	@Test
 	public void emptyOrderTest() {
 		final Order order = new Order();
-		assertEquals(order.size(), 0);
+		assertEquals(order.getShoppingBasket().size(), 0);
+	}
+
+	@Test
+	public void nonEmptyOrderTest() {
+		final Order order = new Order();
+		order.add(new Item("description", BigDecimal.valueOf(10), ItemType.BOOK, true));
+		assertEquals(order.getShoppingBasket().size(), 1);
 	}
 
 }
