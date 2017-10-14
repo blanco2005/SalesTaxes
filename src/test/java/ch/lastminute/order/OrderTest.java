ba4fc4de1134512043d40314bc8be10a7ce20ee9
Francesco Bianchi <blanco20052@gmail.com>
2017-10-14 14:59:14 +0200

    tests refactored

--- src/test/java/ch/lastminute/order/OrderTest.java
+++ src/test/java/ch/lastminute/order/OrderTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.math.BigDecimal;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import ch.lastminute.item.Item;
@@ -11,15 +12,20 @@ import ch.lastminute.item.ItemType;
 
 public class OrderTest {
 
+	Order order;
+
+	@Before
+	public void setUp() {
+		order = new Order();
+	}
+
 	@Test
 	public void emptyOrderTest() {
-		final Order order = new Order();
 		assertEquals(order.getShoppingBasket().size(), 0);
 	}
 
 	@Test
 	public void nonEmptyOrderTest() {
-		final Order order = new Order();
 		order.add(new Item("description", BigDecimal.valueOf(10), ItemType.BOOK, true));
 		assertEquals(order.getShoppingBasket().size(), 1);
 	}
