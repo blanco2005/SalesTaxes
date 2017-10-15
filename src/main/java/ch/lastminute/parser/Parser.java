fb6713412dc4b1447c756d406a6b8cb0ec293502
Francesco Bianchi <blanco20052@gmail.com>
2017-10-15 10:27:24 +0200

    parsing non empty string

--- src/main/java/ch/lastminute/parser/Parser.java
+++ src/main/java/ch/lastminute/parser/Parser.java
@@ -1,14 +1,63 @@
 package ch.lastminute.parser;
 
+import java.math.BigDecimal;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
 import ch.lastminute.item.Item;
+import ch.lastminute.item.ItemType;
 
 public class Parser {
 
 	public List<Item> parse(final String input) {
-		return new LinkedList<>();
+		if (input.isEmpty()) {
+			return new LinkedList<>();
+		}
+		final List<Item> result = new LinkedList<>();
+		final Scanner scanner = new Scanner(input);
+		final Item item = buildItemFromString(scanner.nextLine());
+		result.add(item);
+		scanner.close();
+		return result;
+	}
+
+	private Item buildItemFromString(final String line) {
+
+		final Scanner scanner = new Scanner(line);
+		final int quantiy = scanner.nextInt();
+		boolean imported = false;
+		final StringBuilder sbDescription = new StringBuilder();
+		parse: while (scanner.hasNext()) {
+			final String nextToken = scanner.next();
+			switch (nextToken) {
+			case "imported":
+				imported = true;
+				sbDescription.append(nextToken);
+				break;
+			case "at":
+				break parse;
+			default:
+				sbDescription.append(nextToken);
+			}
+		}
+		final BigDecimal shelfPrice = new BigDecimal(scanner.next());
+		final ItemType type = parseType(sbDescription.toString());
+		scanner.close();
+		return new Item(sbDescription.toString(), shelfPrice, type, imported);
+	}
+
+	private ItemType parseType(final String description) {
+		if (description.contains("chocolate")) {
+			return ItemType.FOOD;
+		}
+		if (description.contains("book")) {
+			return ItemType.BOOK;
+		}
+		if (description.contains("pills")) {
+			return ItemType.MEDICAL;
+		}
+		return ItemType.OTHER;
 	}
 
 }
--- src/test/java/ch/lastminute/parser/ParserTest.java
+++ src/test/java/ch/lastminute/parser/ParserTest.java
@@ -1,8 +1,14 @@
 package ch.lastminute.parser;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.math.BigDecimal;
 
 import org.junit.Test;
+
+import ch.lastminute.item.Item;
+import ch.lastminute.item.ItemType;
 
 public class ParserTest {
 
@@ -11,6 +17,14 @@ public class ParserTest {
 		final String string = "";
 		final Parser parser = new Parser();
 		assertEquals(0, parser.parse(string).size());
+	}
+
+	@Test
+	public void stringWithOneObjectTest() {
+		final Item book = new Item("book", BigDecimal.valueOf(12.49), ItemType.BOOK, false);
+		final String string = "1 book at 12.49";
+		final Parser parser = new Parser();
+		assertTrue(parser.parse(string).contains(book));
 	}
 
 }
