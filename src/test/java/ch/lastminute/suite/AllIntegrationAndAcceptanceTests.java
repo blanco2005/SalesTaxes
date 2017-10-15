61c418280b96f5ea01d340a1c6b7294fa54c8b08
Francesco Bianchi <blanco20052@gmail.com>
2017-10-15 16:40:56 +0200

    added integration test to suite

--- src/test/java/ch/lastminute/suite/AllIntegrationAndAcceptanceTests.java
+++ src/test/java/ch/lastminute/suite/AllIntegrationAndAcceptanceTests.java
@@ -5,11 +5,13 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import ch.lastminute.acceptance.AcceptanceTest;
+import ch.lastminute.integration.order.taxes.IntegrationOrderTaxesTest;
 import ch.lastminute.integration.taxes.rounding.IntegrationTaxesRoundingTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({
 		IntegrationTaxesRoundingTest.class,
+		IntegrationOrderTaxesTest.class,
 		AcceptanceTest.class
 
 })
